8aa96da16e617d363d161ff11b16284c93e4078b
zloadmin <zloadmin@zloadmins-MacBook-Pro.local>
2020-12-03 11:18:06 +0300

    install

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <experimental/filesystem>
+#include <regex>
+#include <iterator>
+
+using namespace cv;
+using namespace std;
+
+namespace fs = std::experimental::filesystem;
+
+
+int main(int argc, char** argv )
+{
+
+}
